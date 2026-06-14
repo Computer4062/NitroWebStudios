465fd566af6c78132d9d8c173b792da3cc123280
Computer4062 <mihan.edirisinghe@gmail.com>
2026-06-14 23:24:33 +0400

    cors changed

--- Dashboard/v1.1/server/index.js
+++ Dashboard/v1.1/server/index.js
@@ -30,7 +30,7 @@ const httpServer = createServer(app);
 
 app.use(cookieParser());
 app.use(cors({
-	origin: 'http://localhost:5173',
+	origin: ['http://localhost:5173', 'https://nitro-web-studios-themes.vercel.app/'],
 	credentials: true
 }));
 app.use(express.json())
