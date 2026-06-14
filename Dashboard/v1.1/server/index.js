2a968ff8a5816fae4600f9c31c02d37a80375380
Computer4062 <mihan.edirisinghe@gmail.com>
2026-06-14 23:47:55 +0400

    changed server resources

--- Dashboard/v1.1/server/index.js
+++ Dashboard/v1.1/server/index.js
@@ -37,7 +37,8 @@ app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(morgan("dev"))
-app.use('/public/uploads', express.static('public/uploads'));
+//app.use('/public/uploads', express.static('public/uploads'));
+app.use('/public/uploads', express.static(path.join(__dirname, 'public/uploads')));
 app.set('etag', false);
 
 // For API calls that require admin/user access: Ex: updating inventory, updating profiles
